01fa8221c988f47d0c0759066cd966ac9a9e1309
sruthi <sruthi@example.com>
2023-09-17 14:34:40 +0530

    aligned home elements according to main padding

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -15,23 +15,23 @@ const Home = () => {
                 </div>
                 <div className='main-container'>
 
-                    <h1 className='sub-heading'>CLASSPASS</h1>
-                    <p className='para center-aligned-para'>
+                    <h1 className='home-heading'>CLASSPASS</h1>
+                    <p className='home-para center-aligned-para'>
                         Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
                         Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
                         ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo,
                         fringilla vel, aliquet nec, vulputate
                     </p>
-                    <h1 className='sub-heading'>WE WORKED TOGETHER ON</h1>
-                    <p className='para'>A campaign to text to go here</p>
+                    <h1 className='home-heading'>WE WORKED TOGETHER ON</h1>
+                    <p className='home-para'>A campaign to text to go here</p>
 
                     <img className="worked-together-img" src="https://res.cloudinary.com/my-cloud123/image/upload/v1694769139/Creative%20skills/ClassPass_Press_Image17_1_ugulrx.png" alt="worked-together-img" />
 
-                    <h1 className='sub-heading'>THE CHALLENGE</h1>
-                    <p className='para center-aligned-para'>
+                    <h1 className='home-heading'>THE CHALLENGE</h1>
+                    <p className='home-para center-aligned-para'>
                         Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate
                     </p>
-                    <p className='para center-aligned-para'>
+                    <p className='home-para center-aligned-para'>
                         Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate
                     </p>
                     <div className='challenge-image-container'>
@@ -39,7 +39,7 @@ const Home = () => {
                         <img className="challenge-image" src="https://res.cloudinary.com/my-cloud123/image/upload/v1694769137/Creative%20skills/ClassPass_Press_Image46_1_kyn29d.png" alt="image2" />
                     </div>
 
-                    <h1 className='sub-heading'>THE RESULTS</h1>
+                    <h1 className='home-heading'>THE RESULTS</h1>
                     <div className='results-container'>
                         <div className='result-detail-container'>
                             <h1 className='result-detail-heading'>347</h1>
@@ -56,7 +56,7 @@ const Home = () => {
                     </div>
                     <img className="result-image" src="https://res.cloudinary.com/my-cloud123/image/upload/v1694769143/Creative%20skills/ClassPass_Press_Image48_1_qhfogu.png" alt="results-img" />
 
-                    <p className='para center-aligned-para'>"R just knocked it out of the park. They conceptualised brilliant campaigns
+                    <p className='home-para center-aligned-para'>"R just knocked it out of the park. They conceptualised brilliant campaigns
                         that took us to the next level, engaging new audiences and driving our business strategy forward."</p>
                     <h2 className='name-title-company-heading'>NAME</h2>
                     <h2 className='name-title-company-heading'>TITLE AND COMPANY NAME</h2>
